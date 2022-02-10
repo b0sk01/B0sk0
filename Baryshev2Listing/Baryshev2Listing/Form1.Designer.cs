d60bad18df81274c1dbc63c31daedb36f3b16c9e
b0sk01 <82368596+b0sk01@users.noreply.github.com>
2022-02-10 16:00:41 +0300

    Add files via upload

---
+++ Baryshev2Listing/Baryshev2Listing/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿
+namespace Baryshev2Listing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Вставка одной строки в другую строку";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(564, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(224, 147);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(9, 225);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(178, 48);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Удаление подстроки из заданной строки ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(9, 279);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(176, 48);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Извлечение подстроки из заданной строки ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(9, 333);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(175, 48);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Определение вхождения подстроки в заданной строке";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(86, 24);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(118, 29);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(86, 59);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(118, 29);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(1, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "ТЕКСТ 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(1, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "ТЕКСТ 2";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(9, 387);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(175, 48);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Преобразование строки в число ";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(193, 171);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(175, 48);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Вставка специального символа";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(193, 225);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(178, 48);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Вставка символа торговой марки ";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(193, 279);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(178, 48);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "Создание строки из повторяющихся символов";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(190, 333);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(178, 48);
+            this.button9.TabIndex = 13;
+            this.button9.Text = "Использование метода String.Format";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(193, 387);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(178, 48);
+            this.button10.TabIndex = 14;
+            this.button10.Text = "Преобразование строки в объект Color и обратно";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(374, 171);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(178, 48);
+            this.button11.TabIndex = 15;
+            this.button11.Text = "Переворачиваем строку при помощи функции Visual Basic";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(377, 225);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(178, 48);
+            this.button12.TabIndex = 16;
+            this.button12.Text = "Переворачиваем строку при помощи C#";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(374, 279);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(178, 48);
+            this.button13.TabIndex = 17;
+            this.button13.Text = "Замена длинного пути многоточием";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(374, 333);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(178, 48);
+            this.button14.TabIndex = 18;
+            this.button14.Text = "Эффект печатающегося текста";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Baryshev2Listing/Baryshev2Listing/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using Microsoft.VisualBasic;
+using System.Runtime.InteropServices;
+
+namespace Baryshev2Listing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string partBookTitle = textBox1.Text;
+            string insertText = textBox2.Text;
+            string bookTitle = partBookTitle.Insert(3, insertText);
+            listBox1.Items.Add(bookTitle);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string bookTitle = textBox1.Text;
+            // Удаляем подстроку с третьей позиции
+            bookTitle = bookTitle.Remove(2);
+            listBox1.Items.Add(bookTitle);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string bookTitle = textBox1.Text;
+            // Извлекаем подстроку с шестой позиции с размером в три символа
+            bookTitle = bookTitle.Substring(5, 3);
+            listBox1.Items.Add(bookTitle);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string str1 = textBox2.Text;
+            string str2 = textBox1.Text;
+            int i = str2.IndexOf(str1);
+            // Проверяем, входит ли строка око в слово Царь-колокол
+            if (i >= 0) listBox1.Items.Add(str1 + " входит в строку " + str2);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string tankman = "4";
+            string dog = "1";
+            textBox1.Text = tankman + dog; // получим 41
+            int all = int.Parse(tankman) + int.Parse(dog);
+            textBox2.Text += Environment.NewLine + all.ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int charCode = 169;
+            char specialChar = Convert.ToChar(charCode);
+            textBox1.Text = specialChar.ToString();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int charCode = 0x00AE;
+            char specialChar = Convert.ToChar(charCode);
+            textBox1.Text += specialChar.ToString();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            // Создаем строку из 5 звездочек
+            System.String FiveStars = new System.String('*', 5);
+            textBox1.Text = FiveStars;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string AutoName;
+            AutoName = "BMW";
+            textBox1.Text =
+            String.Format("Стоимость {0} равна {1:0.0;c}", AutoName, 12000);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            // Задаем цвет
+            Color clr = Color.Blue;
+            // Получим имя выбранного цвета
+            textBox1.Text =
+            (TypeDescriptor.GetConverter(clr).ConvertToString(clr));
+            // Обратная задача. Конвертируем название цвета Green в объект Color
+            clr = (Color)TypeDescriptor.GetConverter(
+            typeof(Color)).ConvertFromString("Blue");
+            // Закрашиваем форму в выбранный цвет
+            this.BackColor = clr;
+        }
+        public void SayHello(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentNullException("name");
+            MessageBox.Show(string.Concat("Hello, ", name));
+        }
+        private void button11_Click(object sender, EventArgs e)
+        {
+            // Взять текст из текстового поля
+            // Например, А роза упала на лапу Азора
+            //string myString = textBox1.Text;
+            // Используем встроенную функцию Visual Basic
+            //textBox2.Text = Strings.StrReverse(myString);
+        }
+
+        public static string ReverseString(string str)
+        {
+            // Проверка на непустоту строки.
+            if (string.IsNullOrEmpty(str))
+            {
+                return str;
+            }
+            // Создадим объект StringBuilder с нужной длиной.
+            StringBuilder revStr = new StringBuilder(str.Length);
+            // Перебираем в цикле все символы
+            // и присоединяем каждый символ к StringBuilder
+            for (int count = str.Length - 1; count > -1; count--)
+            {
+                revStr.Append(str[count]);
+            }
+            // Возвращаем перевернутую строку
+            return revStr.ToString();
+        }
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = ReverseString(textBox1.Text);
+        }
+
+        [DllImport("shlwapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool PathCompactPathEx(
+        System.Text.StringBuilder pszOut,
+        string pszSrc,
+        Int32 cchMax,
+        Int32 dwFlags);
+        private void button13_Click(object sender, EventArgs e)
+        { 
+            // длинный путь к файлу
+            string strPathFile =
+            "c:/program files/My SuperProgram/skins/sample.txt";
+            StringBuilder sb = new StringBuilder(260);
+            // оставляем 20 символов, остальное заменяем многоточием
+            bool b = PathCompactPathEx(sb, strPathFile, 20 + 1, 0);
+            // Выводим результат в текстовое поле
+            textBox1.Text = sb.ToString();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Старт")
+            {
+                timer1.Enabled = true;
+                button14.Text = "Стоп";
+            }
+            else
+            {
+                timer1.Enabled = false;
+            button14.Text = "Старт";
+            }
+        }
+
+        public static int counter = 0;
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            string typingText = textBox1.Text;
+            this.Text = typingText.Substring(0, counter);
+            counter++;
+            if (counter > typingText.Length)
+                counter = 0;
+        }
+    }
+}
